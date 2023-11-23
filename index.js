e86e9677a2854b3bc518893cea39c7d29ed408c8
ArturRochel <arturrochel@gmail.com>
2023-11-23 20:25:59 -0300

    Código Pronto

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Inicio")
+class hero{
+    constructor(name, age, type, gun){
+        this.name = name
+        this.age = age
+        this.type = type
+        this.gun = gun
+    }
+    atacar(){
+        console.log(`O ${this.type} atacou usando ${this.gun}`)
+    }
+}
+
+let guerreiro = new hero("Bilbo", 314, "Guerreiro", "espada")
+let mago = new hero("Gandalf", 1569, "Mago", "magia")
+let monge = new hero("Legolas", 780, "Monge", "artes marciais")
+let ninja = new hero("Samurai", 905, "Ninja", "shuriken")
+
+mago.atacar()
